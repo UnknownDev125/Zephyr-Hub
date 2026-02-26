5e6a9a8d284277b8944626c4c6d5b14023f2d6f8
Unknown | Dev • <107710524+UnknownDev125@users.noreply.github.com>
2026-02-27 01:23:27 +0800

    Create Game-Hub.lua

---
+++ Game-Hub.lua
@@ -0,0 +1,4 @@
+--Get Fat And Jump
+if game.PlaceId == 104901094326217 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/UnknownDev125/Zephyr-Hub/refs/heads/main/GetFatAndFall.lua"))()
+end
