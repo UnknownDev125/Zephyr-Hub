de8f8c48d13e3d02a6addfda46952a84af54bab6
Unknown | Dev • <107710524+UnknownDev125@users.noreply.github.com>
2026-07-19 22:36:40 +0800

    Update Game-Hub.lua

--- Game-Hub.lua
+++ Game-Hub.lua
@@ -2,3 +2,8 @@
 if game.PlaceId == 104901094326217 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/UnknownDev125/Zephyr-Hub/refs/heads/main/GetFatAndFall.lua"))()
 end
+
+--[FPS] One Tap
+if game.PlaceId == 90568084448279 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/UnknownDev125/Zephyr-Hub/refs/heads/main/%5BFPS%5DOne-Tap.lua"))()
+end
